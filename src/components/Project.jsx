e5e85f35379c4acba693cf413d82ea1fed1f90dc
omkeshborse <bomkesh25@gmail.com>
2024-09-09 16:50:45 +0530

    fix : fixed problem of bookmark projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,7 @@ import ProjectCard from "./ProjectCard";
 import SectionTitle from "./SectionTitle";
 function Project() {
   return (
-    <section className="py-20 align-element" id="project">
+    <section className="py-20 align-element" id="projects">
       <SectionTitle text={"web creation"} />
       <div className="py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8">
         {projects.map((project) => {
